2b8dfdb1318d1fe15d2a0aa28d87804bf4d2a510
Óscar Zafra <oskyar@gmail.com>
2013-10-21 11:03:39 +0200

    Añadida contador random, semilla y unas cuantas preguntas más

--- src/main/java/nl/topicus/heroku/wicket/HomePage.java
+++ src/main/java/nl/topicus/heroku/wicket/HomePage.java
@@ -27,21 +27,44 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Locale;
+import java.util.Calendar;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
-
 	public HomePage(final PageParameters parameters) {
+		final ArrayList<String> preguntas= new ArrayList<String>();
+		preguntas.add("¿Qué tipo de virtualización requiere la modificación de los sistemas operativos?");
+		preguntas.add("¿Con qué aplicación hemos restringido, reservado, limintado recursos al sistema?");
+		preguntas.add("¿Qué archivo de configuración teníamos que modificar para dar menos prioridad a los procesos tanto de usuario como de sistema?");
+		preguntas.add("¿Con qué programa se ve la carga real de los programas/procesos y se pueden comprobar los efectos de migración de tareas pesadas?");
+		preguntas.add("¿Con qué orden podíamos saber si nuestro pc soporta virtualización?");
+		preguntas.add("¿Qué gestor de fuentes estamos utilizando actualmente en la asignatura?");
+
+		final ArrayList<String> respuestas= new ArrayList<String>();
+		respuestas.add("paravirtualizacion");
+		respuestas.add("cgroups");
+		respuestas.add("cgcreate");
+		respuestas.add("htop");
+		respuestas.add("kvm-ok");
+		respuestas.add("git");
+
 		final FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
 		add(feedbackPanel);
 		feedbackPanel.setOutputMarkupId(true);
 
-		Form<?> form = new Form("form");
-
-		Label pregunta = new Label(
+//		final int indice = (int)Math.random()*6;
+		Locale locale = new Locale("Spain");
+		Random r = new Random(Calendar.getInstance(locale).getTimeInMillis()*25/100);
+		final int indice = r.nextInt(6);
+		String pregunta = preguntas.get(indice);
+		Label labelPregunta = new Label(
 				"pregunta",
-				"¿Qué tipo de virtualización requiere la modificación de los sistemas operativos?");
-		form.add(pregunta);
+				pregunta);
+		Form<?> form = new Form("form");
+		form.add(labelPregunta);
 
 		final TextField<String> areaRespuesta = new TextField("respuesta", new Model(""));
 		areaRespuesta.setOutputMarkupId(true);
@@ -50,9 +73,9 @@ public class HomePage extends WebPage {
 		Button evaluar = new Button("evaluar") {
 			public void onSubmit() {
 				String respuesta = areaRespuesta.getModelObject();
-				if (respuesta.equals("paravirtualizacion")) {
+				if (respuesta.equals(respuestas.get(indice))) {
 					feedbackPanel
-							.info("¡Enhorabuena! ¡Has aprendido mucho con JJ!");
+							.info("¡Enhorabuena! ¡Has aprendido mucho con JJ!\n Atención a la nueva pregunta");
 				} else {
 					feedbackPanel
 							.error("¡Error! Hay que estudiar un poquito más...");
